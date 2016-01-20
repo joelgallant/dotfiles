9c25b19571b948d849545050c6d36174a0321b93
Joel Gallant <joelgallant236@gmail.com>
2016-01-20 12:02:09 -0700

    Adjusting install script

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
-echo "exec openbox-session" > .xsession
-su; adduser joel sudo; exit
-mkdir documents downloads pictures dev libs
+echo "exec openbox-session" > ~/.xsession
+mkdir ~/documents ~/downloads ~/pictures ~/dev ~/libs
 
-sudo apt-get install openbox x-window-system python-pip git cmake make vim \
-terminator python2.7-dev python3-dev vlc chromium tint2 network-manager-gnome \
-volti conky redshift xscreensaver dmenu pm-utils thunar xarchiver gpicview \
-pavucontrol galculator transmission scrot libreoffice-calc gparted numlockx \
-lightdm vim-gtk feh nodejs nodejs-legacy npm
+sudo apt-get install \
+    openbox x-window-system lightdm tint2 conky feh redshift xscreensaver \
+    git cmake make vim vim-gtk terminator dmenu gparted numlockx \
+    network-manager-gnome thunar volti pm-utils pavucontrol scrot \
+    python-pip python2.7-dev python3-dev nodejs nodejs-legacy npm \
+    chromium vlc xarchiver gpicview galculator transmission libreoffice-calc \
+    libjpeg-dev mupdf
 
 sudo pip install thefuck
 sudo npm install plaidchat -g
@@ -18,6 +19,7 @@ cd libs && git clone https://github.com/Itseez/opencv.git && cd opencv && \
 mkdir build && cd build && cmake .. && sudo make install
 
 sudo dpkg --add-architecture i386
+sudo apt-get update
 
 echo "Next steps:"
 echo "    install firmware-iwlwifi if needed"
